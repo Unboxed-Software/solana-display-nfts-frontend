86907718a79a5c10d61bbe12098725d3b3744e49
john <johnzhengyuliu@gmail.com>
2022-08-30 05:34:37 -0500

    starter

--- src/components/FetchCandyMachine.tsx
+++ src/components/FetchCandyMachine.tsx
@@ -1,99 +1,30 @@
 import { useConnection } from "@solana/wallet-adapter-react"
 import { PublicKey } from "@solana/web3.js"
 import { Metaplex } from "@metaplex-foundation/js"
-import {
-  findCandyMachineByAddressOperation,
-  FindCandyMachineByAddressInput,
-} from "@metaplex-foundation/js"
+import { findCandyMachineByAddressOperation } from "@metaplex-foundation/js"
 import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchCandyMachine: FC = () => {
-  const [candyMachineAddress, setCandyMachineAddress] = useState(
-    "9tQLFyLeaUwQ1PN2YDiFztZDxu4KT6px8CBYEapkshAD"
-  )
+  const [candyMachineAddress, setCandyMachineAddress] = useState(null)
   const [candyMachineData, setCandyMachineData] = useState(null)
   const [pageItems, setPageItems] = useState(null)
   const [page, setPage] = useState(1)
 
-  const { connection } = useConnection()
-  const metaplex = Metaplex.make(connection)
+  const fetchCandyMachine = async () => {}
 
-  // fetch candymachine by address
-  const fetchCandyMachine = async () => {
-    // reset page to 1
-    setPage(1)
+  const getPage = async (page, perPage) => {}
 
-    // fetch candymachine data
-    try {
-      const candyMachine = await metaplex.operations().execute(
-        findCandyMachineByAddressOperation({
-          address: new PublicKey(candyMachineAddress),
-        })
-      )
+  const prev = async () => {}
 
-      let fetchResult = await fetch(candyMachine.items[0].uri)
-      let json = await fetchResult.json()
-      console.log(json)
-
-      setCandyMachineData(candyMachine)
-    } catch (e) {
-      alert("Please submit a valid CMv2 address.")
-    }
-  }
-
-  // paging
-  const getPage = async (page, perPage) => {
-    const pageItems = candyMachineData.items.slice(
-      (page - 1) * perPage,
-      page * perPage
-    )
-
-    // fetch metadata of NFTs for page
-    let nftData = []
-    for (let i = 0; i < pageItems.length; i++) {
-      let fetchResult = await fetch(pageItems[i].uri)
-      let json = await fetchResult.json()
-      nftData.push(json)
-    }
-
-    // set state
-    setPageItems(nftData)
-  }
-
-  // previous page
-  const prev = async () => {
-    if (page - 1 < 1) {
-      setPage(1)
-    } else {
-      setPage(page - 1)
-    }
-  }
-
-  // next page
-  const next = async () => {
-    setPage(page + 1)
-  }
-
-  // fetch placeholder candy machine on load
-  useEffect(() => {
-    fetchCandyMachine()
-  }, [])
-
-  // fetch metadata for NFTs when page or candy machine changes
-  useEffect(() => {
-    if (!candyMachineData) {
-      return
-    }
-    getPage(page, 9)
-  }, [candyMachineData, page])
+  const next = async () => {}
 
   return (
     <div>
       <input
         type="text"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none text-center"
-        placeholder="Enter Candy Machine v2 Address"
+        placeholder="Enter CMv2 Address"
         onChange={(e) => setCandyMachineAddress(e.target.value)}
       />
       <button
