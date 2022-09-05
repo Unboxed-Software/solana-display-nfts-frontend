7e4e3159d92c773d6a7665fe8c3c1663ef37d479
James Pacheco <jamesrp13@gmail.com>
2022-09-05 10:23:25 -0600

    add solution

--- src/components/FetchCandyMachine.tsx
+++ src/components/FetchCandyMachine.tsx
@@ -1,7 +1,7 @@
 import { useConnection } from "@solana/wallet-adapter-react"
 import { PublicKey } from "@solana/web3.js"
 import { Metaplex } from "@metaplex-foundation/js"
-import { FC, useEffect, useState } from "react"
+import { FC, useEffect, useMemo, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchCandyMachine: FC = () => {
@@ -9,14 +9,60 @@ export const FetchCandyMachine: FC = () => {
   const [candyMachineData, setCandyMachineData] = useState(null)
   const [pageItems, setPageItems] = useState(null)
   const [page, setPage] = useState(1)
+  const { connection } = useConnection()
 
-  const fetchCandyMachine = async () => {}
+  const metaplex = useMemo(() => {
+    return Metaplex.make(connection)
+  }, [connection])
 
-  const getPage = async (page, perPage) => {}
+  const fetchCandyMachine = async () => {
+    setPage(1)
 
-  const prev = async () => {}
+    try {
+      const candyMachine = await metaplex
+        .candyMachines()
+        .findByAddress({ address: new PublicKey(candyMachineAddress) })
+        .run()
 
-  const next = async () => {}
+      setCandyMachineData(candyMachine)
+    } catch (error) {
+      alert("Please submit a valid Candy Machine V2 address")
+    }
+  }
+
+  const getPage = async (page, perPage) => {
+    const pageItems = candyMachineData.items.slice(
+      (page - 1) * perPage,
+      page * perPage
+    )
+
+    let nftData = []
+    for (let i = 0; i < pageItems.length; i++) {
+      let fetchResult = await fetch(pageItems[i].uri)
+      let json = await fetchResult.json()
+      nftData.push(json)
+    }
+
+    setPageItems(nftData)
+  }
+
+  const prev = async () => {
+    if (page - 1 < 1) {
+      setPage(1)
+    } else {
+      setPage(page - 1)
+    }
+  }
+
+  const next = async () => {
+    setPage(page + 1)
+  }
+
+  useEffect(() => {
+    if (!candyMachineData) return
+
+    getPage(page, 9)
+  }, [candyMachineData, page])
 
   return (
     <div>
--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -4,7 +4,55 @@ import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchNft: FC = () => {
-  const fetchNfts = async () => {}
+  const [nftData, setNftData] = useState(null)
 
-  return <div></div>
+  const wallet = useWallet()
+  const { connection } = useConnection()
+
+  const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(wallet))
+
+  const fetchNfts = async () => {
+    if (!wallet.connected) {
+      return
+    }
+
+    const nfts = await metaplex
+      .nfts()
+      .findAllByOwner({ owner: wallet.publicKey })
+      .run()
+
+    let nftData = []
+
+    for (let i = 0; i < nfts.length; i++) {
+      let fetchResult = await fetch(nfts[i].uri)
+      let json = await fetchResult.json()
+      nftData.push(json)
+    }
+
+    setNftData(nftData)
+  }
+
+  useEffect(() => {
+    fetchNfts()
+  }, [wallet])
+
+  return (
+    <div>
+      {nftData && (
+        <div className={styles.gridNFT}>
+          {nftData.map((nft) => (
+            <div>
+              <ul>{nft.name}</ul>
+              <img src={nft.image} />
+              <ul>
+                {nft.attributes.map((attribute) => (
+                  <li>{`${attribute.trait_type}: ${attribute.value}`}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
